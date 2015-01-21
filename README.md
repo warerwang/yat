b852b820885668b02cbf1030a5177d24476162ae
warerwang <warerwang@126.com>
2015-01-21 21:28:22 +0800

    第一次提交.

---
+++ README.md
@@ -0,0 +1,5 @@
+这是一个yii2 作为restful api.
+angularjs 做前端的实验项目.
+
+
+yii2
---
+++ config/web.php
@@ -0,0 +1,77 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'x-P22tKADLNJiv0qfwILGjMAHSxBiBOd',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        'user' => [
+            'class' => 'webvimark\modules\UserManagement\components\UserConfig',
+
+            // Comment this if you don't want to record user logins
+            'on afterLogin' => function($event) {
+                    \webvimark\modules\UserManagement\models\UserVisitLog::newVisitor($event->identity->id);
+                }
+        ],
+    ],
+
+    'modules'=>[
+        'user-management' => [
+            'class' => 'webvimark\modules\UserManagement\UserManagementModule',
+
+            // Here you can set your handler to change layout for any controller or action
+            // Tip: you can use this event in any module
+            'on beforeAction'=>function(yii\base\ActionEvent $event) {
+                    if ( $event->action->uniqueId == 'user-management/auth/login' )
+                    {
+                        $event->action->controller->layout = 'loginLayout.php';
+                    };
+                },
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
