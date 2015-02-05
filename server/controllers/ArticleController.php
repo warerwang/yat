6cb6f06c6ceda508a31bca9d431656d2bf7ffc3a
warerwang <warerwang@126.com>
2015-02-06 01:14:45 +0800

    add swagger resource

--- server/controllers/ArticleController.php
+++ server/controllers/ArticleController.php
@@ -12,6 +12,62 @@ use app\components\RestController;
 use app\models\Article;
 use app\models\Category;
 
+/**
+ *
+ * Swagger Annotations
+ * @SWG\Resource(
+ *      resourcePath="/article",
+ *      basePath="/",
+ *      description="article api"
+ * )
+ * @SWG\Model(id="Article", description="文章模型")(
+ *      @SWG\Property(name="id",type="string"),
+ *      @SWG\Property(name="cid",type="string"),
+ *      @SWG\Property(name="title",type="string"),
+ *      @SWG\Property(name="create_time",type="string"),
+ *      @SWG\Property(name="last_modify",type="string"),
+ * )
+ *
+ *
+ * @SWG\Api(
+ *   path="/article",
+ *   description="文章相关接口api",
+ *   @SWG\Operation(
+ *      method="GET",
+ *      type="array",
+ *      @SWG\Items("Article"),
+ *      nickname="list",
+ *      notes="得到文章列表",
+ *   ),
+ *   @SWG\Operation(
+ *      method="POST",
+ *      type="Article",
+ *      nickname="create",
+ *      notes="添加一篇文章",
+ *      @SWG\Parameter(
+ *          name="cid",
+ *          paramType="form",
+ *          required=true,
+ *          type="string",
+ *          description="分类id"
+ *      ),
+ *      @SWG\Parameter(
+ *          name="title",
+ *          paramType="form",
+ *          required=true,
+ *          type="string",
+ *          description="文章标题"
+ *      ),
+ *      @SWG\Parameter(
+ *          name="content",
+ *          paramType="form",
+ *          required=true,
+ *          type="string",
+ *          description="文章描述"
+ *      ),
+ *   ),
+ * )
+ */
 class ArticleController extends RestController
 {
     public function actionIndex ()
