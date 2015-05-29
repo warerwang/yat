3ec6b759080aace95f88a18af7f68f10a7f03efc
yadongw <yadongw@sumilux.cn>
2015-05-29 17:38:44 +0800

    add highlight js

--- client/dev/scripts/controllers/article.js
+++ client/dev/scripts/controllers/article.js
@@ -8,14 +8,16 @@
  * Controller of the webappApp
  */
 angular.module('webappApp')
-  .controller('ArticleCtrl', function ($scope, $http, $routeParams, $rootScope, CategoryServ, ArticleServ) {
+  .controller('ArticleCtrl', function ($scope, $http, $routeParams, $rootScope, CategoryServ, ArticleServ, $sce) {
         $rootScope.breadcrumbs = [
             {href:'/', name:'首页'}
-        ]
+        ];
         var id = $routeParams.id;
         ArticleServ.get({aid:id}, function(article) {
             $scope.article = article;
+	        $scope.article.content = $sce.trustAsHtml($scope.article.content);
             $rootScope.breadcrumbs.push({href:'/category/' + article.cid, name:$scope.getCategory(article.cid).name});
             $rootScope.breadcrumbs.push({href:'#', name:article.title});
+	        $scope.$broadcast('dataloaded');
         });
   });
